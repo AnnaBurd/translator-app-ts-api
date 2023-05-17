4bf8f83b7c662a8f38bc8db9c2b9b916ca27cc05
Anna Burdanova <burdanovaas@gmail.com>
2023-05-17 11:06:09 +0700

    feat: log http requests

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
 import express, { Express, Request, Response } from "express";
 
+import httpLogger from "./utils/http-logger";
+
 const app: Express = express();
 
-app.use("/", (req: Request, res: Response): void => {
+// Follow recommended network security practices
+// if (process.env.NODE_ENV === "production") {
+// app.use(cors());
+// app.use(helmet());
+// app.use(rateLimit({ max: 100, windowMs: 30 * 60 * 1000 }));
+// }
+
+// TODO: check if really needed (after frontend is handled)
+// app.use(mongoSanitize());
+// app.use(xss());
+
+app.use(httpLogger);
+
+app.use("/api/v1/docs", (req: Request, res: Response): void => {
   res.send("Hello world!");
+});
+
+app.all("*", (req: Request, res: Response): void => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find resource at: ${req.url}`,
+  });
 });
 
 export default app;
---
+++ src/utils/http-logger.ts
@@ -0,0 +1,8 @@
+import morgan from "morgan";
+
+import logger from "./logger";
+
+export default morgan(
+  ":remote-addr :method :url -> :status - :response-time ms",
+  { stream: { write: (message: String) => logger.http(message.trim()) } }
+);
